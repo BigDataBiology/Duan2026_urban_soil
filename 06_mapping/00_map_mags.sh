e73a3a2b4eb622d0e3cb32dd003d10649e6d34d8
cocodyq <1057696203@qq.com>
2024-10-17 01:19:27 +0800

    ENH update mapping

--- 06_mapping/00_map_mags.sh
+++ 06_mapping/00_map_mags.sh
@@ -5,4 +5,4 @@
 datapath="/data/yiqian/soil/pipeline/00_quality_control/01_trim_filter_data/"
 name="sample1"
 
-ngless -t . -j 5 run_${name}.ngl ${datapath}/${name}_350.fq_trim_filter.pair.1.fq ${datapath}/${name}_350.fq_trim_filter.pair.2.fq ${datapath}/${name}_350.fq_trim_filter.singles.fq ./08_mapping/mag/final_bins_fa/${name}_final_bins.fa.gz ${name}_mag.sam
+ngless -t . -j 5 map.ngl ${datapath}/${name}_350.fq_trim_filter.pair.1.fq ${datapath}/${name}_350.fq_trim_filter.pair.2.fq ${datapath}/${name}_350.fq_trim_filter.singles.fq ./08_mapping/mag/final_bins_fa/${name}_final_bins.fa.gz ${name}_mag.sam
