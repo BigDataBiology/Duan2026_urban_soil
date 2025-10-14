54c7b4d1568add22aabd0a91479fa64d8acdc8dd
cocodyq <1057696203@qq.com>
2025-10-14 20:10:19 +0800

    MIN update address

--- resource_generation/09_compare_other_db/03_fetchMG.sh
+++ resource_generation/09_compare_other_db/03_fetchMG.sh
@@ -1,3 +1,3 @@
-fetchMGs extraction ~/soil/13_compare/split_soil/${name}_list.txt metagenome ~W/soil/13_compare/marker/all_result_soil/${name}
-fetchMGs extraction ~/soil/13_compare/split_spire/${name}_list.txt metagenome ~W/soil/13_compare/marker/all_result_spire/${name}
-fetchMGs extraction ~/soil/13_compare/split_sgb/${name}_list.txt metagenome ~W/soil/13_compare/marker/all_result_sgb/${name}
+fetchMGs extraction ~/soil/13_compare/split_soil/${name}_list.txt metagenome ~/soil/13_compare/marker/all_result_soil/${name}
+fetchMGs extraction ~/soil/13_compare/split_spire/${name}_list.txt metagenome ~/soil/13_compare/marker/all_result_spire/${name}
+fetchMGs extraction ~/soil/13_compare/split_sgb/${name}_list.txt metagenome ~/soil/13_compare/marker/all_result_sgb/${name}
