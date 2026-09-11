ead26f490e7b4c1d0628ca45a47a05e063ed8eac
indajuan <indajuan@users.noreply.github.com>
2026-09-11 12:36:43 +1000

    plot args as function of Q2 and total nt

--- analysis/supp9/sr_vs_lr.R
+++ analysis/supp9/sr_vs_lr.R
@@ -517,51 +517,14 @@ m <- sr_genes %>% mutate(sum_len = contig_stat$sum_len[match(sample, contig_stat
 write.csv( m %>% dplyr::select(-n), "supp9/number_ARGs_per_sample_SR.csv")
 
 
-plot(m$Q2, m$n)
-plot(log(m$sum_len), m$n)
-
-ggplot(m, aes(x = log(sum_len), y = n, color = City)) +
-  geom_point()
-
-
-ggplot(m, aes(x = Q2, y = n, color = City)) +
-  geom_point()
-
-ggplot(m, aes(x = log(sum_len), y = Q2, color = City)) +
-  geom_point()
-
-
 
 mod_sr  <- lm(n ~ log(sum_len) + log(Q2), data = m)
 summary(mod_sr)
+results_sr_model <- tidy_model(mod_sr)
 
-library(plotly)
 city_levels <- levels(factor(m$City))
-
 m$n_ARGs <- m$n
-plot_ly(m[log(m$sum_len)>20,], x = ~log(sum_len), y = ~log(Q2), z = ~n_ARGs, color = ~City,
-        colors = pal_8[1:length(city_levels)],
-        type = "scatter3d", mode = "markers+lines", marker = list(size = 4)) %>%
-  plotly::layout(scene = list(
-    xaxis = list(title = "log(sum_len)"),
-    yaxis = list(title = "log(Q2)"),
-    zaxis = list(title = "n")
-  ))
 
-
-ggplot(m, aes(x = log(sum_len), y = log(Q2))) +
-  geom_point(aes(size = n_ARGs, fill = n_ARGs, shape = City), color = "black", alpha = 0.8) +
-  scale_fill_fermenter(palette = "YlOrBr", direction = 1, n.breaks = 8) +
-  scale_shape_manual(values = c(21, 24)) +
-  scale_size_continuous(range = c(1, 20)) +
-  labs(
-    x = "log(sum_len)",
-    y = "log(Q2)",
-    size = "n_ARGs",
-    fill = "n_ARGs",
-    shape = "City"
-  ) +
-  theme_minimal()
 
 p_nargs_sr <- ggplot(m, aes(x = log(sum_len), y = log(Q2))) +
   geom_point(aes(size = n_ARGs, fill = n_ARGs, shape = City), color = "black", alpha = 0.8) +
@@ -593,4 +556,34 @@ sr_genes
 lr_genes
 
 
+m <- lr_genes %>% mutate(sum_len = contig_stat_lr$sum_len[match(sample, contig_stat_lr$sample)],
+                         Q2 = contig_stat_lr$Q2[match(sample, contig_stat_lr$sample)]) %>% 
+  group_by(City, Location, sample) %>% summarise(n=n_distinct(centroid), Q2 = Q2[1], sum_len = sum_len[1])
+city_levels <- levels(factor(m$City))
+m$n_ARGs <- m$n
 
+p_nargs_lr <- ggplot(m, aes(x = log(sum_len), y = log(Q2))) +
+  geom_point(aes(size = n_ARGs, fill = n_ARGs, shape = City), color = "black", alpha = 0.8) +
+  ggrepel::geom_text_repel(
+    data = m %>% filter(log(sum_len) < 20),
+    aes(x = log(sum_len), y = log(Q2), label = paste(sample, City, Location)),
+    size = 3, inherit.aes = FALSE
+  ) +
+  scale_fill_fermenter(palette = "YlOrBr", direction = 1, n.breaks = 8) +
+  scale_shape_manual(values = c(21, 24)) +
+  scale_size_continuous(range = c(1, 18)) +
+  labs(
+    x = "log(Total number of bp in assembly)",
+    y = "log(Median contig length)",
+    size = "Number of ARGs",
+    fill = "Number of ARGs",
+    shape = "City"
+  ) +
+  theme_minimal()
+
+svg("supp9/LR_n_args_depth_q2.svg", width = 10, height = 8)
+p_nargs_lr
+dev.off()
+
+
+write.csv( m %>% dplyr::select(-n), "supp9/number_ARGs_per_sample_LR.csv")
